7b94f0c9d0111552c2f3e390a98d55cac3a150d1
WizardGDDG <wizardgd546@gmail.com>
2026-02-27 13:35:14 +0900

    Update Program.cs

--- ConsoleApp11/Program.cs
+++ ConsoleApp11/Program.cs
@@ -1,72 +1,39 @@
 using ScottPlot;
-using System.Drawing;
-using System.Linq;
 using System;
+using System.Linq;
 
 class Program
 {
     static void Main()
     {
-        string papka = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+        string d = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+        
+        Plot p1 = new Plot();
+        for (double x = 0; x <= 6; x += 0.1)
+        for (double y = 0; y <= 6; y += 0.1)
+        if (x + 3*y >= 3 && 2*x + y <= 2 && x + y <= 5)
+            p1.Add.Marker(x, y).Color = Colors.Green.WithAlpha(0.2);
+        
+        double[] t = {0,1,2,3,4,5,6};
+        p1.Add.Scatter(t, t.Select(v => (3-v)/3).ToArray()).Color = Colors.Red;
+        p1.Add.Scatter(t, t.Select(v => 2-2*v).ToArray()).Color = Colors.Blue;
+        
+        p1.Axes.SetLimits(0,6,0,6);
+        p1.SavePng(System.IO.Path.Combine(d,"a.png"),600,600);
 
-        Plot grafik1 = new Plot();
-        for (double a = 0; a <= 6; a += 0.1)
-        {
-            for (double b = 0; b <= 6; b += 0.1)
-            {
-                if (a + 3 * b >= 3 && 2 * a + b <= 2 && a + b <= 5)
-                {
-                    Marker tochka = grafik1.Add.Marker(a, b);
-                    tochka.Color = Colors.Green.WithAlpha(0.2);
-                    tochka.MarkerSize = 2;
-                }
-            }
-        }
-
-        double[] x = { 0, 1, 2, 3, 4, 5, 6 };
-
-        Scatter liniya1 = grafik1.Add.Scatter(x, x.Select(z => (3 - z) / 3).ToArray());
-        liniya1.Color = Colors.Red;
-        Scatter liniya2 = grafik1.Add.Scatter(x, x.Select(z => 2 - 2 * z).ToArray());
-        liniya2.Color = Colors.Blue;
-        Scatter liniya3 = grafik1.Add.Scatter(x, x.Select(z => 5 - z).ToArray());
-        liniya3.Color = Colors.Orange;
-
-        grafik1.Title("система 1");
-        grafik1.XLabel("x1");
-        grafik1.YLabel("x2");
-        grafik1.Axes.SetLimits(0, 6, 0, 6);
-        grafik1.SavePng(System.IO.Path.Combine(papka, "zadanie_a.png"), 600, 600);
-
-        Plot grafik2 = new Plot();
-        for (double q = 0; q <= 10; q += 0.1)
-        {
-            for (double w = 0; w <= 8; w += 0.1)
-            {
-                if (-q + 3 * w <= 9 && 2 * q + 3 * w <= 18 && 2 * q - w <= 10)
-                {
-                    Marker tochka2 = grafik2.Add.Marker(q, w);
-                    tochka2.Color = Colors.Green.WithAlpha(0.2);
-                    tochka2.MarkerSize = 2;
-                }
-            }
-        }
-
-        double[] xx = { 0, 2, 4, 6, 8, 10 };
-
-        Scatter lin1 = grafik2.Add.Scatter(xx, xx.Select(v => (9 + v) / 3).ToArray());
-        lin1.Color = Colors.Red;
-        Scatter lin2 = grafik2.Add.Scatter(xx, xx.Select(v => (18 - 2 * v) / 3).ToArray());
-        lin2.Color = Colors.Blue;
-        Scatter lin3 = grafik2.Add.Scatter(xx, xx.Select(v => 2 * v - 10).ToArray());
-        lin3.Color = Colors.Orange;
-
-        grafik2.Title("система 2");
-        grafik2.XLabel("x1");
-        grafik2.YLabel("x2");
-        grafik2.Axes.SetLimits(0, 10, 0, 8);
-        grafik2.SavePng(System.IO.Path.Combine(papka, "zadanie_b.png"), 600, 600);
-
-        Console.WriteLine("готово, картинки на рабочем столе");
+        Plot p2 = new Plot();
+        for (double x = 0; x <= 10; x += 0.1)
+        for (double y = 0; y <= 8; y += 0.1)
+        if (-x + 3*y <= 9 && 2*x + 3*y <= 18 && 2*x - y <= 10)
+            p2.Add.Marker(x, y).Color = Colors.Green.WithAlpha(0.2);
+        
+        double[] s = {0,2,4,6,8,10};
+        p2.Add.Scatter(s, s.Select(v => (9+v)/3).ToArray()).Color = Colors.Red;
+        p2.Add.Scatter(s, s.Select(v => (18-2*v)/3).ToArray()).Color = Colors.Blue;
+        
+        p2.Axes.SetLimits(0,10,0,8);
+        p2.SavePng(System.IO.Path.Combine(d,"b.png"),600,600);
+        
+        Console.WriteLine("ok");
     }
 }
